2c2ba01190ec8d20aba8967c44ce8832dc0a1ba1
tonykart73 <antoniotartaglia7@gmail.com>
2026-09-15 17:37:29 +0200

    Release KartCoach v0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "it.kartcoach.gps"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.6.0"
     }
 
     kotlinOptions { jvmTarget = "17" }
